f3366dbc3dc40fb1ab5ad1e3f83058edce542fa5
Ashwath Nanda <ashwath5897@gmail.com>
2019-03-06 16:14:07 +0530

    updated for the dataset

--- server.py
+++ server.py
@@ -1,66 +1,34 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+import pandas as pd
+from flashtext import KeywordProcessor
+
 
 app = Flask(__name__)
 api = Api(app)
 
-users = [
-    {
-        "name": "Ashwath",
-        "age": 22,
-        "occupation": "Network Engineer"
-    }
-]
+key_processor = KeywordProcessor()
+data = pd.read_csv('dataset/data_tag.csv')
+l = list(data['Tag'])
+my_list2 = []
+for i in range(len(l)):
+	m = l[i]
+	my_list2.extend(m.split(','))
+	
+#adding the tags onto the processor
+key_processor.add_keywords_from_list(my_list2)
 
 class User(Resource):
-    def get(self, name):
-        for user in users:
-            if(name == user["name"]):
-                return user, 200
-        return "User not found", 404
+   def get(self, name):
+      name.lower()
+      ans = key_processor.extract_keywords(name)
+      ans.sort()
+      ans = ','.join(ans)
+      for i in range(len(l)):
+         if(ans == l[i]):
+            return data['Snippet'][i], 200
+      return 'requirement not found' , 404
 
-    def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if(name == user["name"]):
-                return "User with name {} already exists".format(name), 400
-
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
-
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
-
-        for user in users:
-            if(name == user["name"]):
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-        
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
-
-    def delete(self, name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name), 200
       
 api.add_resource(User, "/user/<string:name>")
 
